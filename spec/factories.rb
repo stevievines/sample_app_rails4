3f1b537d639c8fdd06132679fc531d21962d1721
stevievines <stevievines@gmail.com>
2013-07-11 10:19:49 -0400

    finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name                  "Stevie Vines"
+    email                 "stevie@gmail.com"
+    password              "bwk333"
+    password_confirmation "bwk333"
+  end
+end
